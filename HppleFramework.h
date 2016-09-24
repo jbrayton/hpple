cae71da73ac584d29e7bb11a1aebb9371d9f969b
John Brayton <jbrayton@goldenhillsoftware.com>
2016-09-23 20:25:25 -0400

    umbrella header

---
+++ HppleFramework.h
@@ -0,0 +1,17 @@
+//
+//  HppleFramework.h
+//  HppleFramework
+//
+//  Created by John Brayton on 9/23/16.
+//  Copyright © 2016 John Brayton. All rights reserved.
+//
+
+#ifndef HppleFramework_h
+#define HppleFramework_h
+
+#import "TFHpple.h"
+#import "TFHppleElement.h"
+#import "XPathQuery.h"
+
+
+#endif /* HppleFramework_h */
